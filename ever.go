a851346ddf770436d2db3d00ad87a14124a3ec89
mugund10 <53274246+mugund10@users.noreply.github.com>
2023-07-01 17:13:08 +0530

    first commit

---
+++ ever.go
@@ -0,0 +1,12 @@
+package ever
+
+import "fmt"
+
+func Printever() {
+	fmt.Println("   __                __        __")
+	fmt.Println("  / /    __    __   / /        \\ \\")
+	fmt.Println(" / /___  \\ \\  / /  / /___    ___\\ \\")
+	fmt.Println("(   ___|  \\ \\/ /  (   ___|  (   ___)")
+	fmt.Println(" \\ \\       \\  /    \\ \\       \\ \\")
+	fmt.Println("  \\_\\       \\/      \\_\\       \\_\\")
+}
